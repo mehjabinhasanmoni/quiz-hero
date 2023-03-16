68f89fc808cc2af8bf9fb7c92a6e07511cbfbe74
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-03-16 07:46:43 +0600

    Solved Right Sides Total Marks

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.getElementById('submit').addEventListener('click',function(){
   if (answers.length < 6) {
     return;
   }
@@ -195,3 +195,115 @@ document.querySelector("#submit").addEventlistener("click", () => {
   }, 1500);
   window.scrollTo(0, 0);
 });
+
+// document.getElementById('submit').addEventlistener('click', function(){
+//   if (answers.length < 6) {
+//     return;
+//   }
+//   quizTimer(true);
+//   answersContainer.innerHTML = `<div class="my-4">
+//   <i class="fa-solid fa-fan animate-spin text-2xl text-green-600"></i>
+//   <p class="text-xs animate-pulse">Please Wait, We are checking...</p>
+// </div>`;
+//   let timeTaken = document.querySelector("#count");
+//   let totalMark = 0;
+//   let grade = {
+//     status: "",
+//     color: "",
+//   };
+
+//   for (let ans of answers) {
+ 
+//     if (ans.answer === ans.givenAns) {
+//       totalMark += 10;
+//     }
+//   }
+
+//   if (totalMark === 60) {
+//     grade.status = "Excellent";
+//     grade.color = "text-green-600";
+//   } else if (totalMark >= 40 && totalMark < 60) {
+//     grade.status = "Good";
+//     grade.color = "text-orange-600";
+//   } else {
+//     grade.status = "Poor";
+//     grade.color = "text-red-600";
+//   }
+
+//   // data setting on local storage and getting data from local storage
+//   let storage = JSON.parse(localStorage.getItem("result"));
+//   if (storage) {
+//     localStorage.setItem(
+//       "results",
+//       JSON.stringify([
+//         ...storage,
+//         {
+//           marks: totalMark,
+//           examTime: timeTaken.innerText,
+//           status: grade.status,
+//         },
+//       ])
+//     );
+//   } else {
+//     localStorage.setItem(
+//       "results",
+//       JSON.stringify([
+//         {
+//           marks: totalMark,
+//           examTime: timeTaken.innerText,
+//           status: grade.status,
+//         },
+//       ])
+//     );
+//   }
+
+//   // Right side bar/ answer section
+//   let x = setTimeout(() => {
+//     showAnswers(answers);
+    
+//     displayResult.innerHTML = `<div
+//     class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]"
+//   >
+//     <h3 class="text-xl ${grade.color}">${grade.status}</h3>
+//     <h1 class="text-3xl font-bold my-2">
+//       ${totalMark}<span class="text-slate-800">/60</span>
+//     </h1>
+//     <p class="text-sm flex justify-center items-center gap-2">
+//       Total Time: <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
+//         "sec",
+//         ""
+//       )}<span class="text-xs">sec</span></span>
+//     </p>
+//   </div>
+  
+//   <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
+//   ${
+//     storage
+//       ? `<div class="mt-5">
+//       <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
+//     <div
+//     class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
+//     <div>Marks</div>
+//     <div>Grade</div>
+//     <div>Time</div>
+//     </div>
+//     ${storage
+//       ?.reverse()
+//       ?.map(
+//         (item) => `<div
+//       class="flex justify-between items-center border rounded p-2 my-2 shadow-sm">
+//       <div>${item.marks}/60</div>
+//       <div>${item.status}</div>
+//       <div>${item.examTime}</div>
+//       </div>`
+//       )
+//       ?.join("")}`
+//       : ""
+//   }
+//   </div>
+//   `;
+
+//     clearTimeout(x);
+//   }, 1500);
+//   window.scrollTo(0, 0);
+// });
